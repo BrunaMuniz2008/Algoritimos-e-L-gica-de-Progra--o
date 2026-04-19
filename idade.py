9ccfef8d4fee9e727397670666eb1979c5a2fb9c
BrunaMuniz2008 <brunamuniz2008@gmail.com>
2026-04-19 15:45:26 -0300

    Emulador de Radar de Velocidade com Penalidade

--- idade.py
+++ idade.py
@@ -1,6 +1,18 @@
-idade = int(input("informe sua idade:"))
+velocidade_permitida = float(input("Digite a velocidade permitida na via:"))
+velocidade_registrada = float(input("Digite a velocidade registrada pelo radar:"))
 
-if idade < 18 :
-    print("Você é de menor")
+excesso = velocidade_registrada - velocidade_permitida
+
+if excesso <= 0:
+    print("Motorista dentro do limite!")
 else:
-    print("Você é de maior")
+    porcentagem_excedida = (excesso / velocidade_permitida ) * 100
+
+    if porcentagem_excedida > 20 :
+        print(f"Infração Média \nPorcentagem excedida: {porcentagem_excedida}% \nMulta no valor de R$ {130.16*(porcentagem_excedida/100):.2f}")
+        
+    elif porcentagem_excedida >= 50 :
+        print(f"Infração Grave! \nPorcentagem excedida: {porcentagem_excedida}% \nMulta no valor de R$ {195.23*(porcentagem_excedida/100):.2f}")
+
+    else:
+        print(f"Infração Gravíssima!! \nPorcentagem excedida: {porcentagem_excedida}% \nMulta no valor de R$ {(293.47*(porcentagem_excedida/100)*3):.2f}")
